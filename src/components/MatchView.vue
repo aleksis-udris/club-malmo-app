4a150e32ebbd63b3734ffb75e8baf863e5d02aa5
patriks Laiviņš <patriks.laivin@gmail.com>
2026-06-18 14:21:48 +0200

    Add: Started MatchView

---
+++ src/components/MatchView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="page">
+    <div class="table_container">
+      <table style="margin-top: 200px">
+        <thead>
+          <tr>
+            <th style="padding-right: 50px">Time</th>
+            <th style="padding-right: 100px">Draw</th>
+            <th style="padding-right: 100px">Countries</th>
+            <th style="padding-right: 100px">Score</th>
+            <th style="padding-right: 50px">Court</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>11:00</td>
+            <td>2</td>
+            <td>USA - America</td>
+            <td>6</td>
+            <td>7</td>
+          </tr>
+          <tr>
+            <td>A</td>
+            <td>B</td>
+            <td>C</td>
+            <td>D</td>
+            <td>E</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/HomeView.vue'
 import ClubView from '@/components/ClubView.vue'
+import MatchView from '@/components/MatchView.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/club', component: ClubView },
+  { path: '/match', component: MatchView },
 ]
 
 const router = createRouter({
