7015056783c8fc206555cf08d4834a8d332e4986
patriks Laiviņš <patriks.laivin@gmail.com>
2026-06-18 14:44:45 +0200

    Update: Made table with variables, and made 1 test record for table

--- src/components/MatchView.vue
+++ src/components/MatchView.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const data = [
+  {
+    id: 1,
+    time: "09:00",
+    draw: "avcda",
+    country: "America-USA",
+    score: "1-0",
+    court: "The club of Malmoe"
+  }];
+const matches = ref(data)
+</script>
 
 <template>
   <div class="page">
     <div class="table_container">
-      <table style="margin-top: 200px">
+      <table class="match_table">
         <thead>
           <tr>
             <th style="padding-right: 50px">Time</th>
@@ -14,19 +26,12 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>11:00</td>
-            <td>2</td>
-            <td>USA - America</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <td>A</td>
-            <td>B</td>
-            <td>C</td>
-            <td>D</td>
-            <td>E</td>
+          <tr v-for="match in matches" :key="match.id">
+            <td>{{ match.time }}</td>
+            <td>{{ match.draw }}</td>
+            <td>{{match.country}}</td>
+            <td>{{match.score}}</td>
+            <td>{{match.court}}</td>
           </tr>
         </tbody>
       </table>
